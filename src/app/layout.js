562bca6f7f3358630a54beb3b67d2a0fa68e6c83
Mark <knotzermark@gmail.com>
2025-10-08 01:47:15 +0200

    feat: add Umami analytics script for production environment

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +20,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script
+        async
+        src={process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL}
+        data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+      />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
